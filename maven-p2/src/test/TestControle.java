0f4af4ca3a0c73b691c11ac41787ecca8934e89b
Barbara Moroni <116181111+BaarbaraMoroni@users.noreply.github.com>
2024-11-06 18:09:16 -0300

    update

---
+++ maven-p2/src/test/TestControle.java
@@ -0,0 +1,18 @@
+package test;
+
+import part2.barbara.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.lang.IllegalArgumentException;
+
+class TestControle {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
